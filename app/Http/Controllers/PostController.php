6ce0dc660718c5dbc6f222c905aabb990f791c7f
apaachee2015-maker <apaachee2015@gmail.com>
2026-02-03 00:15:20 +0500

    Last changes 0302

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -52,6 +52,20 @@ class PostController extends Controller
                     'likes' => 12,
                     'is_published' => 1,
                 ],
+                [
+                    'title' => '4  another some title',
+                    'content' => '4  another some content',
+                    'image' => '4 image post',
+                    'likes' => 42,
+                    'is_published' => 1,
+                ],
+                [
+                    'title' => '5 another some title',
+                    'content' => '5 another some content',
+                    'image' => '5 image post',
+                    'likes' => 56,
+                    'is_published' => 1,
+                ],
 
             ];
 
@@ -63,6 +77,81 @@ class PostController extends Controller
                 dd('created');
         }
 
+        public function update() {
+
+            $post = Post::find(6);
+
+            $post->update([
+                'title' => '6 updated some title',
+                'content' => '6 updated some content',
+                'image' => '15 updated image post',
+                'likes' => 65,
+                'is_published' => 1,
+            ]);
+            dd($post->image);
+        }
+
+        public function delete() {
+            $post = Post::withTrashed()->find(7);
+            $post->restore();
+            dd('$post->title');
+        }
+
+        public function firstOrCreate() {
+
+
+           $anotherpost = [
+               'title' => 'some title',
+               'content' => 'some content',
+               'image' => 'some image post',
+               'likes' => 76,
+               'is_published' => 1,
+
+           ];
+
+           $post = Post::firstOrCreate(
+               [
+                   'title' => 'somewooow title',
+               ],
+
+               [
+               'title' => 'wow some title',
+               'content' => 'wow some content',
+               'image' => 'Awesome image post',
+               'likes' => 96,
+               'is_published' => 1,
+           ]);
+
+           dump($post->content);
+
+            dd('finished');
+        }
+
+    public function updateOrCreate()
+    {
+        $anotherpost = [
+            'title' => 'Updatedsome title',
+            'content' => 'Updatedsome content',
+            'image' => 'Updatedsome image post',
+            'likes' => 101,
+            'is_published' => 0,
+
+        ];
+
+
+        $post = Post::updateOrcreate(
+            ['title' => '6 some title'],
+
+            [
+            'title' => '1116 some title',
+            'content' => '11Updatedsome content',
+            'image' => '111 image post',
+            'likes' => 99,
+            'is_published' => 1,
+        ]);
+
+        dd('Updated');
+    }
 
 }
 
